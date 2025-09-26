bcfbd3e7e7d12530aaaf423bcdadc5695e2557ed
Fabricio <fabryleitoh2@gmail.com>
2025-09-25 23:11:19 -0300

    Cree un pequeño formulario

--- src/jsx/Contact.jsx
+++ src/jsx/Contact.jsx
@@ -1,5 +1,26 @@
+import '../Css/Contact.css'
+
 export function Contact() {
   return (
-    <h2>Página de contacto</h2>
+    <form>
+      <h2>Contactanos!</h2>
+      <div>
+        <label htmlFor="Name">Nombre</label>
+        <input type="text" id="Name" name="Name" placeholder="Nombre" />
+      </div>
+      <div>
+        <label htmlFor="Apellido">Apellido</label>
+        <input type="text" id="Apellido" name="Apellido" placeholder="Apellido" />
+      </div>
+      <div>
+        <label htmlFor="Email">Email</label>
+        <input type="email" id="Email" name="Email" placeholder="Email" />
+      </div>
+      <div>
+        <label htmlFor="Message">Mensaje</label>
+        <textarea id="Message" name="Message" placeholder="Dejános tu comentario!" cols={30} rows={5} />
+      </div>
+      <button type="submit">Enviar</button>
+    </form>
   )
 }
