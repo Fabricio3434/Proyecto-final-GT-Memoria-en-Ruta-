755f5fd6eadc7c88c427b895c979cef80b58f54e
Fabricio <fabryleitoh2@gmail.com>
2025-09-27 18:45:06 -0300

    se le dio algunos cambios al form, se agregó el div de redes sociales

--- src/jsx/Contact.jsx
+++ src/jsx/Contact.jsx
@@ -2,25 +2,39 @@ import '../Css/Contact.css'
 
 export function Contact() {
   return (
-    <form>
-      <h2>Contactanos, tu opinion es valiosa!</h2>
-      <div>
-        <label htmlFor="Name">Nombre</label>
-        <input type="text" id="Name" name="Name" placeholder="Nombre" className='Complete' />
+    <div id='container'>
+      <form id='form'>
+        <h2>Contactanos, tu opinion es valiosa!</h2>
+        <div>
+          <label htmlFor="Name">Nombre</label>
+          <input type="text" id="Name" name="Name" placeholder="Nombre"
+            className='complete' />
+        </div>
+        <div>
+          <label htmlFor="Apellido">Apellido</label>
+          <input type="text" id="Apellido" name="Apellido" placeholder="Apellido"
+            className='complete' />
+        </div>
+        <div>
+          <label htmlFor="Email">Email</label>
+          <input type="email" id="Email" name="Email" placeholder="Email"
+            className='complete' />
+        </div>
+        <div>
+          <label htmlFor="Message">Mensaje</label>
+          <textarea id="Message" name="Message" placeholder="Dejános tu comentario!"
+            cols={30} rows={5} className='complete' />
+        </div>
+        <span id='send-span'>
+          <button type="submit" id='send'> Enviar </button>
+        </span>
+      </form>
+
+      <div id='redes'>
+        <h3>O si prefieres, contactanos por nuestras redes sociales!</h3>
+        <div id='redes-buttons'>
+        </div>
       </div>
-      <div>
-        <label htmlFor="Apellido">Apellido</label>
-        <input type="text" id="Apellido" name="Apellido" placeholder="Apellido" className='Complete' />
-      </div>
-      <div>
-        <label htmlFor="Email">Email</label>
-        <input type="email" id="Email" name="Email" placeholder="Email" className='Complete' />
-      </div>
-      <div>
-        <label htmlFor="Message">Mensaje</label>
-        <textarea id="Message" name="Message" placeholder="Dejános tu comentario!" cols={30} rows={5} className='Complete' />
-      </div>
-      <button type="submit">Enviar</button>
-    </form>
+    </div>
   )
 }
