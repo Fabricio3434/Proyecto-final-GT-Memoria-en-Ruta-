53372bd1c985b113a40205c6fa37ca528becbc4d
Fabricio <fabryleitoh2@gmail.com>
2025-10-01 21:58:41 -0300

    Mejora de responsividad, se realizo una navbar responsiva

--- src/jsx/Navbar.jsx
+++ src/jsx/Navbar.jsx
@@ -1,4 +1,5 @@
 import "../css/Navbar.css";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Home, Info, Person, Flatware, Luggage } from '@mui/icons-material';
 import { DinnerSection, TravelSection } from "./Sections.jsx"
@@ -6,30 +7,64 @@ import { Carrusel } from "./carrusel.jsx";
 import { Contact } from "./Contact.jsx";
 
 export function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <nav className="Navbar">
-            <div className="minibar-box">
-                <img src="./img/Navbar-logo.png" className="navbar-logo" alt="" />
+        <>
+            <nav className="Navbar">
+                <div className="minibar-box">
+                    <img src="./img/Navbar-logo.png" className="navbar-logo" alt="" />
+                </div>
+                <h1 className="Título">MEMORIA EN RUTA</h1>
+                <h1 className="título-cel">MER</h1>
+                <div className="navbar-links">
+                    <Link className="navbar-link" to="/home">
+                        <Home sx={{ color: "#E3C16F" }} />
+                    </Link>
+                    <Link className="navbar-link" to="/dinner">
+                        <Flatware sx={{ color: "#E3C16F" }} />
+                    </Link>
+                    <Link className="navbar-link" to="/travel">
+                        <Luggage sx={{ color: "#E3C16F" }} />
+                    </Link>
+                    <Link className="navbar-link" to="/contact">
+                        <Person sx={{ color: "#E3C16F" }} />
+                    </Link>
+                    <Link className="navbar-link" to="/about">
+                        <Info sx={{ color: "#E3C16F" }} />
+                    </Link>
+                </div>
+
+                <button className="hamburger" onClick={() => setIsOpen(!isOpen)}   >
+                    ☰
+                </button>
+
+            </nav>
+
+            <div id="boton-desplegable">
+                <div className={`menu ${isOpen ? "open" : ""}`}>
+                    <Link to="/home" onClick={() => setIsOpen(false)}>
+                        Inicio
+                        <Home sx={{ color: "inherit" }} />
+                    </Link>
+                    <Link to="/dinner" onClick={() => setIsOpen(false)}>
+                        Cenas
+                        <Flatware sx={{ color: "#E3C16F" }} />
+                    </Link>
+                    <Link to="/travel" onClick={() => setIsOpen(false)}>
+                        Viajes
+                        <Luggage sx={{ color: "#E3C16F" }} />
+                    </Link>
+                    <Link to="/contact" onClick={() => setIsOpen(false)}>
+                        Contacto
+                        <Person sx={{ color: "#E3C16F" }} />
+                    </Link>
+                    <Link to="/about" onClick={() => setIsOpen(false)}>
+                        Acerca De
+                        <Info sx={{ color: "#E3C16F" }} />
+                    </Link>
+                </div>
             </div>
-            <h1 className="Título">MEMORIA EN RUTA</h1>
-            <div className="navbar-links">
-                <Link className="navbar-link" to="/home">
-                    <Home sx={{ color: "#E3C16F" }} />
-                </Link>
-                <Link className="navbar-link" to="/dinner">
-                    <Flatware sx={{ color: "#E3C16F" }} />
-                </Link>
-                <Link className="navbar-link" to="/travel">
-                    <Luggage sx={{ color: "#E3C16F" }} />
-                </Link>
-                <Link className="navbar-link" to="/contact">
-                    <Person sx={{ color: "#E3C16F" }} />
-                </Link>
-                <Link className="navbar-link" to="/about">
-                    <Info sx={{ color: "#E3C16F" }} />
-                </Link>
-            </div>
-        </nav>
+        </>
     );
 }
 
