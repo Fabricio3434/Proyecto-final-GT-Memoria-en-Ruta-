3469fceadc8191785ed171c731feb85c1515367d
Lucia <lucia.aime098@gmail.com>
2025-10-07 15:03:22 -0300

    página responsive

--- src/jsx/Acerca.jsx
+++ src/jsx/Acerca.jsx
@@ -83,7 +83,11 @@ ver más
       <h2 id='h2'> Fundadores </h2>
       </div>
       <section id='seccion2'>
-        <Stack direction="row" spacing={6}>
+        <Stack direction={{xs: "column", sm: "row"}} 
+        spacing={6}
+        alignItems="center"
+        justifyContent="center"
+        >
         <Avatar
         sx={{width: 80, height: 80, margin:50, fontSize: 40, backgroundColor: 'orange'}}
         >F</Avatar>
