50bc53a07d3b50c5f2b8f26d9925eeb7de6eae2f
Fabricio <fabryleitoh2@gmail.com>
2025-10-18 17:28:45 -0300

    Se actualizó la configuración de vite.config para que netlify no de fallos

--- src/js/image.js
+++ src/js/image.js
@@ -1,15 +1,15 @@
-import cenas from '../private/cenas.jpg';
-import viajes from '../private/viajes.jpg';
-import empresa from '../private/empresa.jpg';
-import mer from '../private/MemoriaEnRuta.webp'
-import carruselCena from '../private/CarruselCena.webp'
-import carruselViaje from '../private/carruselViaje.webp'
-import cenaBronce from '../private/CenaBronce.webp'
-import cenaPlata from '../private/CenaPlata.webp'
-import cenaOro from '../private/CenaOro.webp'
-import viajeAmérica from '../private/Viaje América.webp'
-import viajeEuropa from '../private/Viaje Europa.webp'
-import viajeOceanía from '../private/Viaje Oceanía.webp'
+import cenas from '@/private/cenas.jpg';
+import viajes from '@/private/viajes.jpg';
+import empresa from '@/private/empresa.jpg';
+import mer from '@/private/MemoriaEnRuta.webp'
+import carruselCena from '@/private/CarruselCena.webp'
+import carruselViaje from '@/private/carruselViaje.webp'
+import cenaBronce from '@/private/CenaBronce.webp'
+import cenaPlata from '@/private/CenaPlata.webp'
+import cenaOro from '@/private/CenaOro.webp'
+import viajeAmérica from '@/private/Viaje América.webp'
+import viajeEuropa from '@/private/Viaje Europa.webp'
+import viajeOceanía from '@/private/Viaje Oceanía.webp'
 
 export {
     cenas, viajes, empresa, mer, carruselCena, carruselViaje, cenaBronce, cenaPlata, cenaOro,
